d3893d04b2fe6e42e914e9924b860e75ef53cd69
Murilo Pistore <91626828+TheBud4@users.noreply.github.com>
2024-07-15 21:58:15 -0300

    Backend Done

---
+++ nlw-frontend-react/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,51 @@
+import { MapPin, Calendar, Settings2, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
+
+interface DestinationAndDateStepProps {
+  isGestsInputOpen: boolean;
+  closeGuestsInput: () => void;
+  openGuestsInput: () => void;
+}
+
+export function DestinationAndDateStep({
+  isGestsInputOpen,
+  closeGuestsInput,
+  openGuestsInput,
+}: DestinationAndDateStepProps) {
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+      <div className="flex items-center flex-1 gap-2">
+        <MapPin className="size-5 text-zinc-400" />
+        <input
+          type="text"
+          disabled={isGestsInputOpen}
+          placeholder="Para onde você vai?"
+          className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+        />
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Calendar className="size-5 text-zinc-400" />
+        <input
+          type="text"
+          disabled={isGestsInputOpen}
+          placeholder="Quando?"
+          className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
+        />
+      </div>
+      <div className="w-px h-6 bg-zinc-800" />
+
+      {isGestsInputOpen ? (
+        <Button onClick={closeGuestsInput} variant="secondary">
+          Alterar local/data
+          <Settings2 className="size-5" />
+        </Button>
+      ) : (
+        <Button onClick={openGuestsInput} variant="primary">
+          Continuar
+          <ArrowRight className="size-5" />
+        </Button>
+      )}
+    </div>
+  );
+}
